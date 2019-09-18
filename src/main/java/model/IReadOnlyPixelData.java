7a6decff81f0492593cd122e87aea01741f889b5
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-18 11:43:15 +0200

    Created a "read only" interface for PixelData class with getters for reading the whole grid and reading indivudual pixels

---
+++ src/main/java/model/IReadOnlyPixelData.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Color;
+import java.util.Iterator;
+
+public interface IReadOnlyPixelData {
+
+  /**
+   * Returns a copy of the matrix of colors (raster).
+   *
+   * @return the matrix of colors.
+   */
+  Iterator<? extends Iterator<Color>> getPixels();
+
+  /**
+   * Returns the color of a specific pixel.
+   *
+   * @param row the row of the pixel zero indexed.
+   * @param col the column of the pixel zero indexed.
+   * @return the color of the pixel.
+   */
+  Color getPixel(int row, int col);
+}
