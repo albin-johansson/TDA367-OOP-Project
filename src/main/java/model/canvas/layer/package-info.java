7d9437f6051b85488930a11d4b2e1cdd7e5502d8
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:48:28 +0200

    Added package-info the the layer package

---
+++ src/main/java/model/canvas/layer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The layer package contains components related to the layer interface.
+ *
+ * @see model.canvas.layer.ILayer
+ * @see model.canvas.layer.LayerFactory
+ */
+package model.canvas.layer;
