425da795b1aa0aa30b06c19bbbcbaeb449ef27ea
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:25:27 +0200

    Added CanvasUpdateListenerComposite class

---
+++ src/test/java/model/canvas/CanvasUpdateListenerCompositeTest.java
@@ -0,0 +1,59 @@
+package model.canvas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CanvasUpdateListenerCompositeTest {
+
+  private CanvasUpdateListenerComposite composite;
+  private int canvasUpdatedTestCount; // used when testing the canvasUpdated-method
+
+  @BeforeEach
+  private void setUp() {
+    composite = new CanvasUpdateListenerComposite();
+    canvasUpdatedTestCount = 0;
+  }
+
+  /**
+   * Creates and returns a generic canvas update listener instance, to be used for testing.
+   *
+   * @return a generic canvas update listener instance, to be used for testing.
+   */
+  private ICanvasUpdateListener createListener() {
+    return () -> {
+      // do nothing when triggered
+    };
+  }
+
+  @Test
+  void add() {
+    assertThrows(NullPointerException.class, () -> composite.add(null));
+
+    // The composite should not allow adding the same listener more than once
+    ICanvasUpdateListener listener = createListener();
+    composite.add(listener);
+    assertThrows(IllegalArgumentException.class, () -> composite.add(listener));
+  }
+
+  @Test
+  void canvasUpdated() {
+    canvasUpdatedTestCount = 0;
+
+    final int firstTerm = 2;
+    final int secondTerm = 5;
+    ICanvasUpdateListener firstListener = () -> canvasUpdatedTestCount += firstTerm;
+    ICanvasUpdateListener secondListener = () -> canvasUpdatedTestCount += secondTerm;
+
+    final int expectedSum = firstTerm + secondTerm;
+
+    composite.add(firstListener);
+    composite.add(secondListener);
+
+    composite.canvasUpdated();
+
+    assertEquals(expectedSum, canvasUpdatedTestCount);
+  }
+}
