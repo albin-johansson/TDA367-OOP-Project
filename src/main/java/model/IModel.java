13e0d6112ff63bf7a6fb657b81e5244f5fced8e0
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:46:47 +0200

    Added ModelImpl and expanded the IModel interface

--- src/main/java/model/IModel.java
+++ src/main/java/model/IModel.java
@@ -8,4 +8,13 @@ import model.canvas.ICanvasUpdateListener;
  */
 public interface IModel {
 
+  /**
+   * Adds a canvas update listener to the model.
+   *
+   * @param listener the listener that will be added, may not be {@code null}.
+   * @throws NullPointerException if any arguments are {@code null}.
+   * @throws IllegalArgumentException if the supplied listener has been added previously.
+   */
+  void addCanvasUpdateListener(ICanvasUpdateListener listener);
+
 }
