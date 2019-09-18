958b8e61cad96268013cab18960cb1bb4caa8bdf
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:46:31 +0200

    Added the ModelFactory class

---
+++ src/test/java/model/ModelFactoryTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class ModelFactoryTest {
+
+  @Test
+  void createModel() {
+    assertNotNull(ModelFactory.createModel());
+  }
+}
