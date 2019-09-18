fc871a6066874aaddcf44b9dd1f53ff2d31254fe
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-18 15:29:14 +0200

    Created test class for pixel data class

---
+++ src/test/java/model/pixeldata/PixelDataTest.java
@@ -0,0 +1,12 @@
+package model.pixeldata;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.awt.Color;
+import org.junit.jupiter.api.Test;
+
+class PixelDataTest {
+}
