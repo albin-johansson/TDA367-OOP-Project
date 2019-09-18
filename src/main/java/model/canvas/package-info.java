01f510583adee54ea4d08addabb56c498779e175
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:49:39 +0200

    Added package-info for the canvas package

---
+++ src/main/java/model/canvas/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The canvas package contains components related to the main canvas, such as layers.
+ *
+ * @see model.canvas.ICanvasUpdateListener
+ * @see model.canvas.layer.ILayer
+ */
+package model.canvas;
