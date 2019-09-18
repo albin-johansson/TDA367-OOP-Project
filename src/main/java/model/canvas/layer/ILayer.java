fd016e788e556d1016abb7ff82b26650dfe70293
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:17:59 +0200

    Added layer interface

---
+++ src/main/java/model/canvas/layer/ILayer.java
@@ -0,0 +1,10 @@
+package model.canvas.layer;
+
+/**
+ * The {@code ILayer} interface specifies objects that represent some sort of "layer" in the
+ */
+public interface ILayer {
+
+  // TODO specify methods...
+
+}
