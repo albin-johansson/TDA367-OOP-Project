af3be734e3bb46d09277bbbf5382045f480ae411
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 15:13:07 +0200

    Added ControllerImplTest

---
+++ src/test/java/controller/ControllerImplTest.java
@@ -0,0 +1,13 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerImplTest {
+
+  @Test
+  void ctor() {
+    assertThrows(NullPointerException.class, () -> new ControllerImpl(null));
+  }
+}
