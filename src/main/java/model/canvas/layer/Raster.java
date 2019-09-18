9ea8b0a9ae05288ba48d09032ee70259218a0fd4
Albin Johansson <albinoscar99@hotmail.com>
2019-09-18 17:43:13 +0200

    Added the Raster class

---
+++ src/main/java/model/canvas/layer/Raster.java
@@ -0,0 +1,11 @@
+package model.canvas.layer;
+
+/**
+ * The {@code Raster} class is an implementation of the {@code ILayer} interface that represents a
+ * layer that contains an arbitrary collection of pixels.
+ */
+final class Raster implements ILayer {
+
+  // TODO implement...
+
+}
