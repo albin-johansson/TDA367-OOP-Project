1b917debf38311ca4d381b6175fff7dc0ebb60c7
Herman Bergstrom <hermanb@student.chalmers.se>
2019-09-19 15:10:04 +0200

    Removed redundant import in PimpApp.java

--- src/main/java/PimpApp.java
+++ src/main/java/PimpApp.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import model.IModel;
 import model.ModelFactory;
-import model.canvas.ICanvasUpdateListener;
 import view.IView;
 import view.ViewFactory;
 
