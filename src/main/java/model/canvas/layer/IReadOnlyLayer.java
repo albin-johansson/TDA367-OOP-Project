4bb20cf6e00558ff431507e78d7215f040b35e53
Albin Johansson <albinoscar99@hotmail.com>
2019-09-19 17:35:21 +0200

    Added the IReadOnlyLayer interface

---
+++ src/main/java/model/canvas/layer/IReadOnlyLayer.java
@@ -0,0 +1,38 @@
+package model.canvas.layer;
+
+import model.pixeldata.IReadOnlyPixelData;
+
+/**
+ * The {@code IReadOnlyLayer} interface specifies the basic read-only methods for layers.
+ */
+public interface IReadOnlyLayer {
+
+  /**
+   * Indicates whether or not the layer is visible.
+   *
+   * @return {@code true} if the layer is visible; {@code false} otherwise.
+   */
+  boolean isVisible();
+
+  /**
+   * Returns the x-coordinate of the layer.
+   *
+   * @return the x-coordinate of the layer.
+   */
+  int getX();
+
+  /**
+   * Returns the y-coordinate of the layer.
+   *
+   * @return the y-coordinate of the layer.
+   */
+  int getY();
+
+  /**
+   * Returns a pixel representation of this layer, which is read-only.
+   *
+   * @return a pixel representation of this layer.
+   */
+  IReadOnlyPixelData getPixelData();
+
+}
