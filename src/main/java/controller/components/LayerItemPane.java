53a3c7a00e0dc0911a337a2a2a9f6b8b98d713d2
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2019-09-20 10:31:14 +0200

    LayerItemPane created

---
+++ src/main/java/controller/components/LayerItemPane.java
@@ -0,0 +1,36 @@
+package controller.components;
+
+import controller.ControllerUtils;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import util.Resources;
+
+public class LayerItemPane extends AnchorPane {
+
+  @FXML
+  @SuppressWarnings("unused")
+  private TextField textField;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private ToggleButton toggleButton;
+
+  @FXML
+  @SuppressWarnings("unused")
+  private ImageView imageView;
+
+  /**
+   * @throws IOException if the associated FXML file cannot be found.
+   */
+  public LayerItemPane() throws IOException {
+    ControllerUtils.makeController(this, Resources.find(getClass(), "layer_item.fxml"));
+    setStyle("-fx-background-color: gray;");
+  }
+}
