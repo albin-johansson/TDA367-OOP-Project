eb46fc18f6c0acca01ca80fa68396e66f986931a
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-24 11:06:15 +0200

    Created rectangle skeleton class.

---
+++ src/main/java/model/canvas/layer/Rectangle.java
@@ -0,0 +1,72 @@
+package model.canvas.layer;
+
+import java.awt.Color;
+import model.pixeldata.IReadOnlyPixelData;
+
+/**
+ * The {@code Rectangle} class is an implementation of the {@code ILayer} interface that represents
+ * a rectangle.
+ */
+public class Rectangle implements ILayer {
+
+  private int width;
+  private int height;
+  private final LayerDelegate layerDelegate;
+  private static final LayerType layerType = LayerType.SHAPE;
+
+  /**
+   * Creates a rectangle.
+   *
+   * @param width the width of the rectangle.
+   * @param height the height of the rectangle.
+   */
+  Rectangle(int width, int height) {
+    this.layerDelegate = new LayerDelegate();
+    this.width = width;
+    this.height = height;
+  }
+
+  @Override
+  public void setPixel(int x, int y, Color color) {
+  }
+
+  @Override
+  public void setVisible(boolean isVisible) {
+    layerDelegate.setVisible(isVisible);
+  }
+
+  @Override
+  public void setX(int x) {
+    layerDelegate.setX(x);
+  }
+
+  @Override
+  public void setY(int y) {
+    layerDelegate.setY(y);
+  }
+
+  @Override
+  public boolean isVisible() {
+    return layerDelegate.isVisible();
+  }
+
+  @Override
+  public int getX() {
+    return layerDelegate.getX();
+  }
+
+  @Override
+  public int getY() {
+    return layerDelegate.getY();
+  }
+
+  @Override
+  public IReadOnlyPixelData getPixelData() {
+    return null;
+  }
+
+  @Override
+  public LayerType getLayerType() {
+    return layerType;
+  }
+}
