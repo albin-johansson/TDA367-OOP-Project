3c61081c7f973494a3f5e76e62d5d4ec19ef7fe2
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-24 09:45:31 +0200

    Removed content in pencil test as it required javafx mouse events and had no other public methods

--- src/test/java/model/tools/PencilTest.java
+++ src/test/java/model/tools/PencilTest.java
@@ -1,32 +1,10 @@
 package model.tools;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.awt.Color;
-import model.canvas.layer.ILayer;
-import model.canvas.layer.LayerFactory;
-import org.junit.jupiter.api.Test;
-
+/**
+ * TODO: Pencil class might be too similar to a controller. Hard to test the pencil class as it
+ * requires javafx mouse events and has no other public methods. Refactor the pencil class in the
+ * future and add tests then when it is possible to create better tests.
+ */
 class PencilTest {
 
-  @Test
-  void drawTest() {
-    Pencil pencil = new Pencil(2, Color.BLACK);
-    ILayer layer = LayerFactory.createRasterLayer(100, 100);
-    pencil.setTarget(layer);
-    pencil.draw(10, 10);
-    int nBlackPixels = 0;
-
-    for (Iterable<Color> row : layer.getPixelData().getPixels()) {
-      for (Color color : row) {
-        if (color.getRGB() == Color.BLACK.getRGB()) {
-          nBlackPixels++;
-        }
-      }
-    }
-
-    assertEquals(nBlackPixels, 4, "Should draw a 2x2 pixel grid.");
-    assertEquals(layer.getPixelData().getPixel(10, 10).getRGB(), Color.BLACK.getRGB(),
-        "Pixel should be colored black.");
-  }
 }
