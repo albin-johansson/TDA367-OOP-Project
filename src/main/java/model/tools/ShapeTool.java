b9e0dca4de3ab47cec74e21949fc837c8043e7a0
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-24 11:19:24 +0200

    Created ShapeTool class skeleton

---
+++ src/main/java/model/tools/ShapeTool.java
@@ -0,0 +1,30 @@
+package model.tools;
+
+import javafx.scene.input.MouseEvent;
+import model.canvas.layer.ILayer;
+
+/**
+ * A tool for creating and editing tools.
+ */
+public class ShapeTool implements ITool {
+
+  @Override
+  public void dragged(MouseEvent mouseEvent) {
+
+  }
+
+  @Override
+  public void pressed(MouseEvent mouseEvent) {
+
+  }
+
+  @Override
+  public void released(MouseEvent mouseEvent) {
+
+  }
+
+  @Override
+  public void setTarget(ILayer iLayer) {
+
+  }
+}
