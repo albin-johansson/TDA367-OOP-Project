a49f6e199f72ef17047c00cc7967453bf5e032dd
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2019-09-24 09:58:00 +0200

    Made corresponding variables in controller

--- src/main/java/controller/components/LayerItemManagerPane.java
+++ src/main/java/controller/components/LayerItemManagerPane.java
@@ -1,5 +1,26 @@
 package controller.components;
 
-public class LayerItemManagerPane {
+import controller.ControllerUtils;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import util.Resources;
 
+public class LayerItemManagerPane extends AnchorPane {
+
+  @FXML
+  private StackPane stackPane;
+
+  @FXML
+  private AnchorPane emptyLayerPane;
+
+  @FXML
+  private VBox layerItemVBox;
+
+  LayerItemManagerPane() throws IOException {
+    ControllerUtils.makeController(this, Resources.find(getClass(), "layer_item_manager.fxml"));
+    setStyle("-fx-background-color: gray;");
+  }
 }
