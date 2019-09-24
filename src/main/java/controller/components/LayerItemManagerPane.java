622f6edeead067005f3e03d4c1a59f4d7fa8bce9
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2019-09-24 11:43:47 +0200

    Added create and add-LayerItemPane methods

--- src/main/java/controller/components/LayerItemManagerPane.java
+++ src/main/java/controller/components/LayerItemManagerPane.java
@@ -6,8 +6,13 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import model.canvas.layer.IReadOnlyLayer;
 import util.Resources;
 
+/**
+ * The {@code LayerItemManagerPane} class manages the layer items in the view. Listens to the model
+ * for updates.
+ */
 public class LayerItemManagerPane extends AnchorPane {
 
   @FXML
@@ -20,17 +25,38 @@ public class LayerItemManagerPane extends AnchorPane {
   @FXML
   private VBox layerItemVBox;
 
+  /**
+   * @throws IOException if the associated FXML file cannot be found.
+   */
   LayerItemManagerPane() throws IOException {
     ControllerUtils.makeController(this, Resources.find(getClass(), "layer_item_manager.fxml"));
     setStyle("-fx-background-color: gray;");
   }
 
-  public void addLayerItemPane(LayerItemPane layerItemPane){
+  
+
+  /**
+   * Creates the LayerItems for the view, based on a {@code IReadOnlyLayer}
+   *
+   * @param layer the {@code IReadOnlyLayer} that will be created as a view component
+   * @return the corresponding {@code LayerItemPane} created from the {@code IReadOnlyLayer}
+   */
+  private LayerItemPane createLayerItemPane(IReadOnlyLayer layer) {
+    try {
+      return new LayerItemPane();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * @param layerItemPane the layerItemPane to add to the VBox for display
+   */
+  private void addLayerItemPane(LayerItemPane layerItemPane) {
     try {
       layerItemVBox.getChildren().add(layerItemPane);
-    }catch (Exception e){
-      //TBA
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-
   }
 }
