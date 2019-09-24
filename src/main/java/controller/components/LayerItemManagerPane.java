d1754adfce2b7a25483159efa51b1cd6964ed788
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2019-09-24 10:05:10 +0200

    Frame for addLayerItemPane method added

--- src/main/java/controller/components/LayerItemManagerPane.java
+++ src/main/java/controller/components/LayerItemManagerPane.java
@@ -16,11 +16,21 @@ public class LayerItemManagerPane extends AnchorPane {
   @FXML
   private AnchorPane emptyLayerPane;
 
+  //Should the itemManager have real layers or just the view aspect?
   @FXML
   private VBox layerItemVBox;
 
   LayerItemManagerPane() throws IOException {
     ControllerUtils.makeController(this, Resources.find(getClass(), "layer_item_manager.fxml"));
     setStyle("-fx-background-color: gray;");
+  }
+
+  public void addLayerItemPane(LayerItemPane layerItemPane){
+    try {
+      layerItemVBox.getChildren().add(layerItemPane);
+    }catch (Exception e){
+      //TBA
+    }
+
   }
 }
