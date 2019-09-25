0981fd9332d33aa6ff705ff10265096a50f8841d
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-25 13:00:57 +0200

    Made shape tool private and made contructor package private

--- src/main/java/model/tools/ShapeTool.java
+++ src/main/java/model/tools/ShapeTool.java
@@ -6,7 +6,13 @@ import model.canvas.layer.ILayer;
 /**
  * A tool for creating and editing shapes.
  */
-public class ShapeTool implements ITool {
+public final class ShapeTool implements ITool {
+
+  /**
+   * Creates and returns a shape tool.
+   */
+  ShapeTool() {
+  }
 
   @Override
   public void dragged(MouseEvent mouseEvent) {
