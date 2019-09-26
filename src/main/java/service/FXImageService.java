8c67f4f4703f75c2859cb07e9a465bb5441a974e
Herman Bergstrom <hermanb@student.chalmers.se>
2019-09-26 16:06:42 +0200

    Created FXImageServiceClass

--- src/main/java/service/FXImageService.java
+++ src/main/java/service/FXImageService.java
@@ -1,5 +1,15 @@
 package service;
 
+import javafx.scene.image.Image;
+
+/**
+ * The {@code FXImageService} class is a service which takes pixeldata and returns an FX Image
+ */
 public class FXImageService {
+
+  public static Image getFXImage(){
+    
+    return null;
+  }
 
 }
