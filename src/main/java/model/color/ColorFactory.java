872d3d9a15199a4755a6f44c7694acf03b4c4d7d
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-26 17:31:26 +0200

    Implemented basic factory for colors

--- src/main/java/model/color/ColorFactory.java
+++ src/main/java/model/color/ColorFactory.java
@@ -3,14 +3,14 @@ package model.color;
 public class ColorFactory {
 
   public static IColor createColor(int red, int green, int blue, int alpha) {
-    return null;
+    return new ColorImp(red, green, blue, alpha);
   }
 
   public static IColor createColor(int red, int green, int blue) {
-    return null;
+    return new ColorImp(red, green, blue, 1);
   }
 
   public static IColor createColor(String hex) {
-    return null;
+    return new ColorImp(hex);
   }
 }
