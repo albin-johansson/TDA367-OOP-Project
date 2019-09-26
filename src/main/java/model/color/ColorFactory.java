c6eede094eeb4a37e62ed726a17c478138731ba9
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-26 09:40:36 +0200

    Created skeleton for color factory

---
+++ src/main/java/model/color/ColorFactory.java
@@ -0,0 +1,16 @@
+package model.color;
+
+public class ColorFactory {
+
+  public static IColor createColor(int red, int green, int blue, int alpha) {
+    return null;
+  }
+
+  public static IColor createColor(int red, int green, int blue) {
+    return null;
+  }
+
+  public static IColor createColor(String hex) {
+    return null;
+  }
+}
