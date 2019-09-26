8f6fdd4e15d875c4dea9b92fe3ba9a97e75f2139
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-26 12:41:42 +0200

    Created skeleton for color implementations test class

---
+++ src/test/java/model/color/ColorImpTest.java
@@ -0,0 +1,74 @@
+package model.color;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ColorImpTest {
+
+  private ColorImp colorImp;
+
+  @BeforeEach
+  void setUp() {
+    colorImp = new ColorImp(0, 0, 0, 1);
+  }
+
+  @Test
+  void setColor() {
+  }
+
+  @Test
+  void setRed() {
+  }
+
+  @Test
+  void setGreen() {
+  }
+
+  @Test
+  void setBlue() {
+  }
+
+  @Test
+  void setPercentageRed() {
+  }
+
+  @Test
+  void setPercentageGreen() {
+  }
+
+  @Test
+  void setPercentageBlue() {
+  }
+
+  @Test
+  void getRed() {
+  }
+
+  @Test
+  void getGreen() {
+  }
+
+  @Test
+  void getBlue() {
+  }
+
+  @Test
+  void getRedPercentage() {
+  }
+
+  @Test
+  void getGreenPercentage() {
+  }
+
+  @Test
+  void getBluePercentage() {
+  }
+
+  @Test
+  void getAlphaHexCode() {
+  }
+
+  @Test
+  void getHexCode() {
+  }
+}
