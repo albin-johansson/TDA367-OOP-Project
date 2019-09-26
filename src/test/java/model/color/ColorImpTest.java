2a7371fe97d2c27fb61f92d95fd5a6a7b6358e17
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-26 15:21:04 +0200

    Added tests for setting the colors seperately

--- src/test/java/model/color/ColorImpTest.java
+++ src/test/java/model/color/ColorImpTest.java
@@ -1,15 +1,21 @@
 package model.color;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ColorImpTest {
 
   private ColorImp colorImp;
+  private int val;
+  private double percentage;
 
   @BeforeEach
   void setUp() {
     colorImp = new ColorImp(0, 0, 0, 1);
+    val = 10;
+    percentage = val / 255.0;
   }
 
   @Test
@@ -18,14 +24,35 @@ class ColorImpTest {
 
   @Test
   void setRed() {
+    colorImp.setRed(val);
+    assertEquals(colorImp.getRed(), val);
+    assertEquals(colorImp.getRedPercentage(), percentage);
+    colorImp.setRed(-val);
+    assertEquals(colorImp.getRed(), 0);
+    colorImp.setRed(256);
+    assertEquals(colorImp.getRed(), 255);
   }
 
   @Test
   void setGreen() {
+    colorImp.setGreen(val);
+    assertEquals(colorImp.getGreen(), val);
+    assertEquals(colorImp.getGreenPercentage(), percentage);
+    colorImp.setGreen(-val);
+    assertEquals(colorImp.getGreen(), 0);
+    colorImp.setGreen(256);
+    assertEquals(colorImp.getGreen(), 255);
   }
 
   @Test
   void setBlue() {
+    colorImp.setBlue(val);
+    assertEquals(colorImp.getBlue(), val);
+    assertEquals(colorImp.getBluePercentage(), percentage);
+    colorImp.setBlue(-val);
+    assertEquals(colorImp.getBlue(), 0);
+    colorImp.setBlue(256);
+    assertEquals(colorImp.getBlue(), 255);
   }
 
   @Test
