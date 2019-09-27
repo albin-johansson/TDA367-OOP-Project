6c743308cb314d26c3935a6c3f6864f1308fcc25
Theo Wiik <theo.le.wiik@gmail.com>
2019-09-27 14:35:12 +0200

    Added javadoc to color factory

--- src/main/java/model/color/ColorFactory.java
+++ src/main/java/model/color/ColorFactory.java
@@ -1,22 +1,54 @@
 package model.color;
 
 /**
- * Creates colors.
+ * The {@code ColorFactory} class is a factory for creating instances of the {@code IColor}
+ * interface.
  */
-public class ColorFactory {
+public final class ColorFactory {
 
+  private ColorFactory() {
+
+  }
+
+  /**
+   * Creates and returns a transparent color.
+   *
+   * @return a transparent color.
+   */
   public static IColor createColor() {
     return new ColorImpl(0, 0, 0, 0);
   }
 
+  /**
+   * Creates and returns a transparent color.
+   *
+   * @param red   the red component in the range [0, 255].
+   * @param green the green component in the range [0, 255].
+   * @param blue  the blue component in the range [0, 255].
+   * @param alpha the alpha component in the range [0, 255].
+   * @return a color specified by the given color components.
+   */
   public static IColor createColor(int red, int green, int blue, int alpha) {
     return new ColorImpl(red, green, blue, alpha);
   }
 
+  /**
+   * Creates and returns a transparent color.
+   *
+   * @param red   the red component in the range [0, 255].
+   * @param green the green component in the range [0, 255].
+   * @param blue  the blue component in the range [0, 255].
+   * @return a color specified by the given color components and no transparency.
+   */
   public static IColor createColor(int red, int green, int blue) {
     return new ColorImpl(red, green, blue, 1);
   }
 
+  /**
+   * Sets a color by its hex value.
+   * @param hex the hexadecimal value of the color.
+   * @return a color specified by the hexadecimal color.
+   */
   public static IColor createColor(String hex) {
     return new ColorImpl(hex);
   }
