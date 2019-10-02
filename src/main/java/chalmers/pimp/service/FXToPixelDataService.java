0409c0a1d4509dfa791a5c05341317149fa2a98e
davidm <damoller99@hotmail.se>
2019-10-02 17:28:03 +0200

    Added documentation to FXToPixelDataService

--- src/main/java/chalmers/pimp/service/FXToPixelDataService.java
+++ src/main/java/chalmers/pimp/service/FXToPixelDataService.java
@@ -5,11 +5,20 @@ import chalmers.pimp.model.pixeldata.PixelFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 
+/**
+ * Converts an javafx.Image to PixelData
+ */
 public final class FXToPixelDataService {
 
   private FXToPixelDataService() {
   }
 
+  /**
+   * Converts an javafx.Image to PixelData
+   *
+   * @param image the image to be converted
+   * @return the resulting pixel data
+   */
   public static PixelData getImage(Image image) {
 
     int width = (int) image.getWidth();
@@ -31,13 +40,20 @@ public final class FXToPixelDataService {
     return pixelData;
   }
 
+  /**
+   * Converts a 32-bit int containing pixel color into an array of doubles with color info
+   *
+   * @param argb the 32-bit int to be converted. Each color channel is specified in each byte
+   * @return [0] = blue percentage, [1] = green percentage, [2] = red percentage, [3] = alpha
+   * percentage
+   */
   private static double[] argbToArgbArr(int argb) {
-    double[] arr = new double[4];
+    double[] percentage = new double[4];
 
     for (int i = 0; i < 4; i++) {
-      arr[i] = (argb & 0xFF) / 255.0;
+      percentage[i] = (argb & 0xFF) / 255.0;
       argb >>= 8;
     }
-    return arr;
+    return percentage;
   }
 }
