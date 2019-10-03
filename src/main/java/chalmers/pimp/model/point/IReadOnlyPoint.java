29c5f4700b782ff66a65979cf96b46533f1f6cdc
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-03 13:32:54 +0200

    Created a point class

---
+++ src/main/java/chalmers/pimp/model/point/IReadOnlyPoint.java
@@ -0,0 +1,21 @@
+package chalmers.pimp.model.point;
+
+/**
+ * The {@code IReadOnlyPoint} represent a point that can be read.
+ */
+public interface IReadOnlyPoint {
+
+  /**
+   * Returns the x coordinate for the point.
+   *
+   * @return the x coordinate for the point.
+   */
+  int getX();
+
+  /**
+   * Returns the y coordinate for the point.
+   *
+   * @return the y coordinate for the point.
+   */
+  int getY();
+}
---
+++ src/main/java/chalmers/pimp/model/point/Point.java
@@ -0,0 +1,41 @@
+package chalmers.pimp.model.point;
+
+/**
+ * The {@code Point} is a implementation of the {@code IPoint} interface.
+ */
+public final class Point implements IPoint {
+
+  private int x;
+  private int y;
+
+  /**
+   * Creates and returns a new point.
+   *
+   * @param x the x coordinate.
+   * @param y the y coordinate.
+   */
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  @Override
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  @Override
+  public int getX() {
+    return x;
+  }
+
+  @Override
+  public int getY() {
+    return y;
+  }
+}
