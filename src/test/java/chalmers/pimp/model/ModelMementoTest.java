ad9e42898441c9205f0c6660d9ef04d0fdf6fe68
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-15 10:41:57 +0200

    Updated model memento test to take in a color

--- src/test/java/chalmers/pimp/model/ModelMementoTest.java
+++ src/test/java/chalmers/pimp/model/ModelMementoTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import chalmers.pimp.model.canvas.CanvasFactory;
 import chalmers.pimp.model.canvas.CanvasMemento;
 import chalmers.pimp.model.canvas.ICanvas;
+import chalmers.pimp.model.color.Colors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,12 +19,12 @@ class ModelMementoTest {
   void setUp() {
     ICanvas canvas = CanvasFactory.createCanvas();
     canvasMemento = canvas.createSnapShot();
-    modelMemento = new ModelMemento(canvasMemento);
+    modelMemento = new ModelMemento(canvasMemento, Colors.BLACK);
   }
 
   @Test
   void ctor() {
-    assertThrows(NullPointerException.class, () -> new ModelMemento(null));
+    assertThrows(NullPointerException.class, () -> new ModelMemento(null, null));
   }
 
   @Test
