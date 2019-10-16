6dacbb19666ab45f77f5ff51e020763ebf45f2ae
theowiik <theo.le.wiik@gmail.com>
2019-10-16 19:15:08 +0200

    add javadoc to createColorModel in color color model factory

--- src/main/java/chalmers/pimp/model/color/colormodel/ColorModelFactory.java
+++ src/main/java/chalmers/pimp/model/color/colormodel/ColorModelFactory.java
@@ -13,8 +13,14 @@ public final class ColorModelFactory {
   private ColorModelFactory() {
   }
 
+  /**
+   * Creates and returns a implementation of the IColorModel interface.
+   *
+   * @param color the color the color model should hold.
+   * @return a color model with the provided color.
+   * @throws NullPointerException if the provided color is {@code null}.
+   */
   public static IColorModel createColorModel(IColor color) {
-    Objects.requireNonNull(color);
-    return new ColorModelImpl(color);
+    return new ColorModelImpl(Objects.requireNonNull(color));
   }
 }
