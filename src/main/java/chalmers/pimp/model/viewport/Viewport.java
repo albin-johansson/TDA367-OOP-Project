54a26d9a3b7ea18a12d7d67b99fbb60a31695570
Albin Johansson <albinoscar99@hotmail.com>
2019-10-16 08:55:07 +0200

    Added Viewport class

---
+++ src/main/java/chalmers/pimp/model/viewport/Viewport.java
@@ -0,0 +1,143 @@
+package chalmers.pimp.model.viewport;
+
+import java.util.Objects;
+
+/**
+ * The {@code Viewport} class is an implementation of the {@code IReadOnlyViewport} interface that
+ * also provides package-private setters.
+ *
+ * @see IReadOnlyViewport
+ */
+final class Viewport implements IReadOnlyViewport {
+
+  private static final int DEFAULT_WIDTH = 100;
+  private static final int DEFAULT_HEIGHT = 100;
+
+  private int x;
+  private int y;
+  private int width;
+  private int height;
+
+  /**
+   * Creates a viewport with the dimensions {@value Viewport#DEFAULT_WIDTH}x{@value
+   * Viewport#DEFAULT_HEIGHT}.
+   */
+  Viewport() {
+    x = 0;
+    y = 0;
+    width = DEFAULT_WIDTH;
+    height = DEFAULT_HEIGHT;
+  }
+
+  /**
+   * Creates a copy of the supplied viewport.
+   *
+   * @param viewport the viewport that will be copied.
+   * @throws NullPointerException if the supplied viewport is {@code null}.
+   */
+  Viewport(Viewport viewport) {
+    Objects.requireNonNull(viewport);
+    x = viewport.x;
+    y = viewport.y;
+    width = viewport.width;
+    height = viewport.height;
+  }
+
+  /**
+   * Centers the viewport over the are with the supplied dimensions.
+   *
+   * @param areaWidth  the width of the area.
+   * @param areaHeight the height of the area.
+   */
+  void center(int areaWidth, int areaHeight) {
+    int x = (areaWidth - width) / 2;
+    int y = (areaHeight - height) / 2;
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Moves the viewport with the supplied offsets.
+   *
+   * @param dx the x-axis movement, may be negative.
+   * @param dy the y-axis movement, may be negative.
+   */
+  void move(int dx, int dy) {
+    x += dx;
+    y += dy;
+  }
+
+  /**
+   * Sets the x-coordinate of the viewport.
+   *
+   * @param x the new x-coordinate of the viewport.
+   */
+  void setX(int x) {
+    this.x = x;
+  }
+
+  /**
+   * Sets the y-coordinate of the viewport.
+   *
+   * @param y the new y-coordinate of the viewport.
+   */
+  void setY(int y) {
+    this.y = y;
+  }
+
+  /**
+   * Sets the width of the viewport.
+   *
+   * @param width the new width of the viewport, must be greater than zero.
+   * @throws IllegalArgumentException if the supplied width isn't greater than zero.
+   */
+  void setWidth(int width) {
+    if (width < 1) {
+      throw new IllegalArgumentException("Invalid viewport width: " + width);
+    }
+    this.width = width;
+  }
+
+  /**
+   * Sets the height of the viewport.
+   *
+   * @param height the new height of the viewport, must be greater than zero.
+   * @throws IllegalArgumentException if the supplied height isn't greater than zero.
+   */
+  void setHeight(int height) {
+    if (height < 1) {
+      throw new IllegalArgumentException("Invalid viewport height: " + height);
+    }
+    this.height = height;
+  }
+
+  @Override
+  public int getX() {
+    return x;
+  }
+
+  @Override
+  public int getY() {
+    return y;
+  }
+
+  @Override
+  public int getWidth() {
+    return width;
+  }
+
+  @Override
+  public int getHeight() {
+    return height;
+  }
+
+  @Override
+  public int getRelativeX(int x) {
+    return this.x + x;
+  }
+
+  @Override
+  public int getRelativeY(int y) {
+    return this.y + y;
+  }
+}
---
+++ src/test/java/chalmers/pimp/model/viewport/ViewportTest.java
@@ -0,0 +1,143 @@
+package chalmers.pimp.model.viewport;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ViewportTest {
+
+  private Viewport viewport;
+
+  @BeforeEach
+  void setUp() {
+    viewport = new Viewport();
+  }
+
+  @Test
+  void center() {
+    int width = 212;
+    int height = 512;
+
+    int areaWidth = 124;
+    int areaHeight = 617;
+
+    viewport.setWidth(width);
+    viewport.setHeight(height);
+
+    viewport.center(areaWidth, areaHeight);
+
+    assertEquals((areaWidth - width) / 2, viewport.getX());
+    assertEquals((areaHeight - height) / 2, viewport.getY());
+  }
+
+  @Test
+  void move() {
+    int x = 1124;
+    int y = 8125;
+    int dx = 124;
+    int dy = -25;
+
+    viewport.setX(x);
+    viewport.setY(y);
+
+    viewport.move(dx, dy);
+
+    assertEquals((x + dx), viewport.getX());
+    assertEquals((y + dy), viewport.getY());
+  }
+
+  @Test
+  void setX() {
+    assertEquals(0, viewport.getX());
+
+    int x = -3193;
+    viewport.setX(x);
+
+    assertEquals(x, viewport.getX());
+  }
+
+  @Test
+  void setY() {
+    assertEquals(0, viewport.getY());
+
+    int y = 2345;
+    viewport.setY(y);
+
+    assertEquals(y, viewport.getY());
+  }
+
+  @Test
+  void setWidth() {
+    assertDoesNotThrow(() -> viewport.setWidth(1));
+    assertThrows(IllegalArgumentException.class, () -> viewport.setWidth(0));
+  }
+
+  @Test
+  void setHeight() {
+    assertDoesNotThrow(() -> viewport.setHeight(1));
+    assertThrows(IllegalArgumentException.class, () -> viewport.setHeight(0));
+  }
+
+  @Test
+  void getX() {
+    assertEquals(0, viewport.getX());
+
+    int x = 9124;
+    viewport.setX(x);
+
+    assertEquals(x, viewport.getX());
+  }
+
+  @Test
+  void getY() {
+    assertEquals(0, viewport.getY());
+
+    int y = 3294;
+    viewport.setY(y);
+
+    assertEquals(y, viewport.getY());
+  }
+
+  @Test
+  void getWidth() {
+    assertEquals(100, viewport.getWidth());
+
+    int width = 8132;
+    viewport.setWidth(width);
+
+    assertEquals(width, viewport.getWidth());
+  }
+
+  @Test
+  void getHeight() {
+    assertEquals(100, viewport.getHeight());
+
+    int height = 92;
+    viewport.setHeight(height);
+
+    assertEquals(height, viewport.getHeight());
+  }
+
+  @Test
+  void getRelativeX() {
+    int x = 924;
+    int viewportX = 124;
+    int expected = x + viewportX;
+
+    viewport.setX(viewportX);
+    assertEquals(expected, viewport.getRelativeX(x));
+  }
+
+  @Test
+  void getRelativeY() {
+    int y = -125;
+    int viewportY = 516;
+    int expected = y + viewportY;
+
+    viewport.setY(viewportY);
+    assertEquals(expected, viewport.getRelativeY(y));
+  }
+}
