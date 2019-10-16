86c736a4004c469bacabaaba5013e3d5703da447
Albin Johansson <albinoscar99@hotmail.com>
2019-10-16 22:08:20 +0200

    Improve readability of Doodle

--- src/main/java/chalmers/pimp/model/canvas/layer/Doodle.java
+++ src/main/java/chalmers/pimp/model/canvas/layer/Doodle.java
@@ -5,6 +5,7 @@ import chalmers.pimp.model.Point;
 import chalmers.pimp.model.color.IReadOnlyColor;
 import chalmers.pimp.model.pixeldata.IPixel;
 import chalmers.pimp.model.pixeldata.IReadOnlyPixelData;
+import chalmers.pimp.model.viewport.IReadOnlyViewport;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,36 +23,37 @@ final class Doodle implements ILayer {
   private int height;
 
   /**
-   * Creates a layer which has a list of points and draws straight lines between the points.
-   *
-   * @param lineWidth the width of the line between the points
-   * @param color     the color of the lines
-   * @throws NullPointerException if color is null
+   * @param lineWidth the width of the lines that are drawn.
+   * @param color     the color of the doodle layer.
+   * @throws NullPointerException if the supplied color is {@code null}.
    */
   Doodle(int lineWidth, IReadOnlyColor color) {
-    points = new ArrayList<>();
-    layerDelegate = new LayerDelegate(LayerType.DOODLE);
-    layerDelegate.setName("Doodle");
     this.lineWidth = lineWidth;
     this.color = Objects.requireNonNull(color);
+
+    points = new ArrayList<>(16);
+    layerDelegate = new LayerDelegate(LayerType.DOODLE);
+    layerDelegate.setName("Doodle");
     width = 0;
     height = 0;
   }
 
   /**
-   * Creates a copy of the specified doodle
+   * Creates a copy of the supplied doodle.
    *
-   * @param doodle the specified doodle to be copied
-   * @throws NullPointerException if color is null
+   * @param doodle the doodle that will be copied.
+   * @throws NullPointerException if the supplied doodle is {@code null}.
    */
-  Doodle(Doodle doodle) throws NullPointerException {
+  private Doodle(Doodle doodle) {
     Objects.requireNonNull(doodle);
-    this.layerDelegate = new LayerDelegate(doodle.layerDelegate);
-    points = new ArrayList<>();
+
+    layerDelegate = new LayerDelegate(doodle.layerDelegate);
+    points = new ArrayList<>(doodle.points.size());
+    points.addAll(doodle.points);
     color = doodle.color;
     lineWidth = doodle.lineWidth;
-
-    doodle.points.forEach(p -> points.add(p));
+    width = doodle.width;
+    height = doodle.height;
   }
 
   @Override
@@ -112,8 +114,7 @@ final class Doodle implements ILayer {
 
   @Override
   public IReadOnlyPixelData getPixelData() {
-    //TODO: convert the lines to PixelData
-    return null;
+    return null; // FIXME remove
   }
 
   @Override
@@ -147,7 +148,7 @@ final class Doodle implements ILayer {
   }
 
   @Override
-  public void draw(IRenderer renderer) {
+  public void draw(IRenderer renderer, IReadOnlyViewport viewport) {
     if (!isVisible() || points.isEmpty()) {
       return;
     }
@@ -157,14 +158,23 @@ final class Doodle implements ILayer {
     renderer.setBorderColor(color);
 
     if (points.size() == 1) {
-      renderer.drawLine(points.get(0).addX(getX()).addY(getY()),
-          points.get(0).addX(getX()).addY(getY()));
+      Point point = points.get(0);
+      point = point.addX(viewport.getRelativeX(getX()));
+      point = point.addY(viewport.getRelativeY(getY()));
+      renderer.drawLine(point, point); // basically renders a single point
       return;
     }
 
     for (int i = 1; i < points.size(); i++) {
-      renderer.drawLine(points.get(i).addX(getX()).addY(getY()),
-          points.get(i - 1).addX(getX()).addY(getY()));
+      Point point1 = points.get(i);
+      point1 = point1.addX(viewport.getRelativeX(getX()));
+      point1 = point1.addY(viewport.getRelativeY(getY()));
+
+      Point point2 = points.get(i - 1);
+      point2 = point2.addX(viewport.getRelativeX(getX()));
+      point2 = point2.addY(viewport.getRelativeY(getY()));
+
+      renderer.drawLine(point1, point2);
     }
   }
 }
