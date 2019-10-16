c343a0a907391965b1f1c7456739410542a74d0c
Albin Johansson <albinoscar99@hotmail.com>
2019-10-16 21:03:14 +0200

    Add TODO about moving ILayerUpdateListener

--- src/main/java/chalmers/pimp/model/canvas/ILayerUpdateListener.java
+++ src/main/java/chalmers/pimp/model/canvas/ILayerUpdateListener.java
@@ -4,7 +4,7 @@ package chalmers.pimp.model.canvas;
  * The {@code ILayerUpdateListener} interface specifies object that may listen for updates of the
  * layers.
  */
-public interface ILayerUpdateListener {
+public interface ILayerUpdateListener { // TODO move to layer package
 
   /**
    * Invoked when the layers has been updated.
