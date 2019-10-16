29d62a99623c740a744210471a89f0a856f868b5
Albin Johansson <albinoscar99@hotmail.com>
2019-10-16 22:22:05 +0200

    Tweak view package-info

--- src/main/java/chalmers/pimp/view/package-info.java
+++ src/main/java/chalmers/pimp/view/package-info.java
@@ -1,6 +1,5 @@
 /**
- * The model package contains components related to the model aspect of
- * the Pimp application.
+ * The model package contains components related to the model aspect of the Pimp application.
  *
  * @see chalmers.pimp.view.IView
  * @see chalmers.pimp.view.ViewFactory
