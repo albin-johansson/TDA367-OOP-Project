131af4e8984b0ba26e3777623039995ef0c90fc4
Theo Wiik <theo.le.wiik@gmail.com>
2019-10-17 13:57:11 +0200

    Create TextTool.java

---
+++ src/main/java/chalmers/pimp/model/tools/TextTool.java
@@ -0,0 +1,22 @@
+package chalmers.pimp.model.tools;
+
+import chalmers.pimp.model.MouseStatus;
+
+/**
+ * The {@code TextTool} is a implementation of the {@code ITool} interface that creates and edits
+ * texts.
+ */
+final class TextTool implements ITool {
+
+  @Override
+  public void dragged(MouseStatus mouseStatus) {
+  }
+
+  @Override
+  public void pressed(MouseStatus mouseStatus) {
+  }
+
+  @Override
+  public void released(MouseStatus mouseStatus) {
+  }
+}
